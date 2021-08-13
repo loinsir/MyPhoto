e66f983109ec05c4c55889de78d973da6091f673
loinsir <a9327370@gmail.com>
2021-08-13 20:24:41 +0900

    Initial Commit

---
+++ MyPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPhoto
+//
+//  Created by 김인환 on 2021/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
